09c113477f5d32d9d2641c9d88bf7a383cfb3f4c
Treasure Chinyimba <Treasurearthur4@gmail.com>
2024-02-20 20:24:01 +0200

    reduce method

--- datatypes.js
+++ datatypes.js
@@ -1,5 +1,5 @@
 "use strict";
 let numList;
-numList = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-let result = numList.filter((num) => num > 3);
+numList = [1, 2, 3, 4, 5];
+let result = numList.reduce((acc, num) => num + acc);
 console.log(result);
