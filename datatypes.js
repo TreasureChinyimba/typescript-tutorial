79733dadf6c420c34371a944e7138e8fcc8ec8cb
Treasure Chinyimba <Treasurearthur4@gmail.com>
2024-02-20 20:16:24 +0200

    first commit

---
+++ datatypes.js
@@ -0,0 +1,5 @@
+"use strict";
+let numList;
+numList = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let result = numList.filter((num) => num > 3);
+console.log(result);
---
+++ datatypes.ts
@@ -0,0 +1,6 @@
+let numList : Array<number>;
+numList = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+let result = numList.filter((num)=> num >3)
+
+console.log(result);
