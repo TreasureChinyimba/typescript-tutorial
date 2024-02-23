f769e69d9d70b0528113419672a7cebca27b58f8
Treasure Chinyimba <Treasurearthur4@gmail.com>
2024-02-23 15:57:49 +0200

    enum

--- datatypes.js
+++ datatypes.js
@@ -3,3 +3,10 @@ let numList;
 numList = [1, 2, 3, 4, 5];
 let result = numList.reduce((acc, num) => num + acc);
 console.log(result);
+var color;
+(function (color) {
+    color[color["red"] = 0] = "red";
+    color[color["blue"] = 1] = "blue";
+    color[color["green"] = 2] = "green";
+})(color || (color = {}));
+let b = color.red;
--- datatypes.ts
+++ datatypes.ts
@@ -4,3 +4,12 @@ numList = [1, 2, 3, 4, 5];
 let result = numList.reduce((acc, num)=> num+acc)
 
 console.log(result);
+
+
+enum color {
+    red,
+    blue,
+    green
+}
+
+let b : color = color.red;
