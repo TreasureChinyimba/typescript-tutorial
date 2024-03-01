628758961714f3b7ad284651992d8c7d3571ba8d
Treasure Chinyimba <Treasurearthur4@gmail.com>
2024-03-01 22:37:32 +0200

    update

--- datatypes.js
+++ datatypes.js
@@ -1,6 +1,2 @@
 "use strict";
-let numList;
-numList = [1, 2, 3, 4, 5];
-let result = numList.reduce((acc, num) => num + acc);
-console.log(result);
-let b = 0 /* color.red */;
+Object.defineProperty(exports, "__esModule", { value: true });
--- datatypes.ts
+++ datatypes.ts
@@ -1,15 +1,1 @@
-let numList : Array<number>;
-numList = [1, 2, 3, 4, 5];
-
-let result = numList.reduce((acc, num)=> num+acc)
-
-console.log(result);
-
-
-const enum color {
-    red,
-    blue,
-    green
-}
-
-let b : color = color.red;
+import { question } from "readline-sync";
