c19a6e5641698c9c61c50cc790f52882e7e31d64
Treasure Chinyimba <Treasurearthur4@gmail.com>
2024-03-01 22:44:46 +0200

    update

--- datatypes.js
+++ datatypes.js
@@ -1,2 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const readline_sync_1 = require("readline-sync");
+function main() {
+    const firstString = (0, readline_sync_1.question)('Enter first number:/n');
+    console.log(firstString);
+}
+main();
--- datatypes.ts
+++ datatypes.ts
@@ -1,1 +1,8 @@
 import { question } from "readline-sync";
+
+function main():void {
+    const firstString: string = question('Enter first number:/n');
+    console.log(firstString);
+    
+}
+main();
